wtrsltnk/zyn

base func initially working.

src/Params/ADnoteParameters.h
@@ -36,7 +36,7 @@ class OscilGen;
 class FFTwrapper;
 
 enum FMTYPE {
-    NONE, MORPH, RING_MOD, PHASE_MOD, FREQ_MOD, PITCH_MOD
+    NONE, MORPH, RING_MOD, PHASE_MOD, FREQ_MOD, WAVE_MOD, PITCH_MOD
 };
 extern int ADnote_unison_sizes[];
 

petersalomonsen/zynaddsubfx

src/Params/ADnoteParameters.h
@@ -18,7 +18,7 @@
 #include "PresetsArray.h"
 
 enum FMTYPE {
-    NONE, MORPH, RING_MOD, PHASE_MOD, FREQ_MOD, PW_MOD
+    NONE, MORPH, RING_MOD, PHASE_MOD, FREQ_MOD, PW_MOD, WAVE_MOD
 };
 
 /*****************************************************************/